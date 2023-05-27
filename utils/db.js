18b4d718282441f4b3c44c1fc8b299480c3f1eb9
kalibetre <kalbetre@icloud.com>
2023-05-28 00:30:02 +0300

    add utility functions to get user by email and add user

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import sha1 from 'sha1';
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
@@ -48,6 +49,39 @@ class DBClient {
    */
   async nbFiles() {
     return this.db.collection('files').countDocuments();
+  }
+
+  /**
+   * Finds a user by their email in the "users" collection.
+   *
+   * @param {string} email - The email of the user to find.
+   * @return {Promise} A Promise that resolves with the user object, or null if
+   * not found.
+   */
+  findUserByEmail(email) {
+    return this.db.collection('users').findOne({ email });
+  }
+
+  /**
+   * Adds a new user to the database with the given email and password.
+   *
+   * @param {string} email - The email of the user to add.
+   * @param {string} password - The password of the user to add.
+   * @return {Object} The user object that was added to the database, with the
+   * password and _id fields removed.
+   */
+  async addUser(email, password) {
+    const hashedPassword = sha1(password);
+    const result = await this.db.collection('users').insertOne(
+      {
+        email,
+        password: hashedPassword,
+      },
+    );
+    result.ops[0].id = result.ops[0]._id;
+    delete result.ops[0].password;
+    delete result.ops[0]._id;
+    return result.ops[0];
   }
 }
 
