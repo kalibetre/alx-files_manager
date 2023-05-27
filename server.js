289d08fc9ba4f9e818db996430677658995efe09
kalibetre <kalbetre@icloud.com>
2023-05-28 00:29:43 +0300

    enable json parsing on express js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import router from './routes';
 
 const app = express();
+app.use(express.json());
 app.use(router);
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5001);
 
 export default app;
