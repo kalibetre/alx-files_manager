ae6daea644fa4381d658c10bd988b12542f8e246
kalibetre <kalbetre@icloud.com>
2023-05-29 00:25:32 +0300

    allow larger files to be sent via json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routes';
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(router);
 app.listen(process.env.PORT || 5000);
 
