401dfb6e0f1dcf8db9a32b02638b9bc439900847
kalibetre <kalbetre@icloud.com>
2023-05-28 22:21:54 +0300

    add auth controller end points

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 
 const router = Router();
@@ -7,5 +8,8 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 
 export default router;
