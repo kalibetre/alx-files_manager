065c927292b01e6bb9db04d364f570976c581375
kalibetre <kalbetre@icloud.com>
2023-05-29 01:16:16 +0300

    use json with default limit

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import router from './routes';
 
 const app = express();
-app.use(express.json({ limit: '50mb' }));
+// app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(router);
 app.listen(process.env.PORT || 5000);
 
