587711a7c80a4a873dd7a1a81aea219dea48490f
kalibetre <kalbetre@icloud.com>
2023-05-29 00:25:18 +0300

    add files route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
 const router = Router();
@@ -11,5 +12,6 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
+router.post('/files', FilesController.postUpload);
 
 export default router;
