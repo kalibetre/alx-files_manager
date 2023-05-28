c40398e618e3bfc0d83bb257b24b7961f90a01ed
kalibetre <kalbetre@icloud.com>
2023-05-28 22:22:08 +0300

    change default port to 5000

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import router from './routes';
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(process.env.PORT || 5001);
+app.listen(process.env.PORT || 5000);
 
 export default app;
